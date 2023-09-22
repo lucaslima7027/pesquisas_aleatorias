4547907cac4bdbd487a3423334410f23d430020b
Lucas Lima <limalucas7027@gmail.com>
2023-09-22 20:02:31 -0300

    Change language to english

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-async function palavraAleatoria() {
+async function rndWord() {
     const response = await fetch("https://api.dicionario-aberto.net/random");
     const dicionario = await response.json();
     return dicionario.word;
@@ -6,10 +6,10 @@ async function palavraAleatoria() {
 
 
 
-function abreAbas() {
+function openTab() {
 
     for (let index = 0; index < 10; index++) {
-        palavraAleatoria().then((result) => {
+        rndWord().then((result) => {
             var link = "https://www.bing.com/search?q="+result
             window.open(link, "_blank");
         });

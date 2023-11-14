f3fb5b608639b6ee7194a9712ae56d7dbcbda451
Lucas Lima <limalucas7027@gmail.com>
2023-11-14 09:20:05 -0300

    Fixing url

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function abreAbas() {
 
     for (let index = 0; index < 10; index++) {
         palavraAleatoria().then((result) => {
-            var link = "https://www.bing.com/search?q="+result
+            var link = `https://www.bing.com/search?q=${result}&form=ANSPH1&refig=f841cb20db31406bb46822c319837104&pc=U531`
             window.open(link, "_blank");
         });
         
